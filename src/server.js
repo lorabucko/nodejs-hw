748a47969b99ec44ceb4de8822cef3ed2c7559c7
lorabucko <lorabucko@gmail.com>
2026-08-20 14:12:17 +0200

    pino-pretty added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 // src/server.js
 import express from 'express';
 import dotenv from 'dotenv';
+import 'dotenv/config';
 import cors from 'cors';
-import pinoHttp  from 'pino-http';
+import pino  from 'pino-http';
 
 dotenv.config();
 
@@ -11,7 +12,21 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 // повертає всі нотатки
 app.get('/notes', (req, res) => {
@@ -22,9 +37,8 @@ app.get('/notes', (req, res) => {
 
 // повертає одну нотатку за ID
   app.get('/notes/:noteId', (req, res) => {
-
   res.status(200).json({
-	"message": "Retrieved note with ID: ${req.params.noteId}}"
+	message: "Retrieved note with ID: ${req.params.noteId}}"
 });
 });
 
@@ -43,7 +57,6 @@ app.use(((req, res, next) => {
 
 // Middleware для обробки помилок (останнє)
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
   res.status(500).json({
     message: "Simulated server error",
     error: err.message,
