b9106f9e8bbaa7b892c2b4397ed3a9cc04b3ef9f
lorabucko <lorabucko@gmail.com>
2026-08-20 11:54:20 +0200

    server is created

---
+++ src/server.js
@@ -0,0 +1,45 @@
+// src/server.js
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// повертає всі нотатки
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+	"message": "Retrieved all notes"
+});
+});
+
+// повертає одну нотатку за ID
+  app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({
+	"message": "Retrieved note with ID: id_param"
+});
+});
+
+//  Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 для обробки неiснуючих маршрутів
+app.use(((req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found'
+  });
+}));
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
